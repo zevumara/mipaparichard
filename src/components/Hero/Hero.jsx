46ebc0ccee402f726b271d54571f09729026c6d0
zevamaru <sebastiancamara@gmail.com>
2024-10-30 20:41:51 -0300

    Nuevas imágenes, correciones de texto, versión desktop.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,19 +2,42 @@ import React from "react";
 import { FaHeart } from "react-icons/fa";
 import ElObservador from "../../assets/el-observador.webp";
 import Subrayado from "../../assets/subrayado.webp";
+import MiPapaRichard from "../../assets/mipaparichard.webp";
+import Secuelas from "../../assets/mipaparichard_secuelas.webp";
 import { FaRegFilePdf } from "react-icons/fa6";
+import { useState } from "react";
 
 const Hero = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleShow = () => setIsVisible(true);
+  const handleClose = () => setIsVisible(false);
+
+  const downloadPDF = async () => {
+    const response = await fetch("/Paciente_Alonso_Richard_int._27.09.24.pdf"); // Cambia esta ruta según el origen
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "Paciente_Alonso_Richard_int._27.09.24.pdf");
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  };
+
   return (
     <>
-      <div className="container px-6 py-4 text-sm">
+      <div className="container px-6 py-4 max-w-[720px]">
         <div className="space-y-4">
           <p>
             El pasado 19 de abril de 2024, cerca de las 6 de la mañana,{" "}
             <strong>
-              mi papá, Richard Alonso, de 46 años, iba camino a su trabajo cuando fue atropellado
-              por un patrullero de la policía que circulaba sin sirena y en contramano a altas
-              velocidades
+              mi papá, Richard Alonso Godoy, de 46 años, iba camino a su trabajo cuando{" "}
+              <span className="bg-yellow-300">
+                fue atropellado por un patrullero de la policía que circulaba sin sirena y en
+                contramano a muy altas velocidades
+              </span>
             </strong>
             . El impacto fue devastador. Sufrió una lesión en la primera vértebra que comprometió
             seriamente su movilidad, junto con otras complicaciones neurológicas.
@@ -47,19 +70,25 @@ const Hero = () => {
             <span className="font-bold text-sky-700">María Agustina Alonso</span>
           </p>
         </div>
-        <button className="bg-red-600 w-full my-8 py-2 text-white font-bold rounded text-lg md:hidden flex justify-center items-center shadow-md">
+        <img src={MiPapaRichard} className="mt-4 rounded shadow-md" />
+        <button
+          onClick={handleShow}
+          className="bg-red-600 w-full my-6 py-2 text-white font-bold rounded text-lg flex justify-center items-center shadow-md"
+        >
           Donar <FaHeart className="ml-2" />
         </button>
-        <div className="space-y-4">
-          <h2 className="font-['Caveat'] text-4xl">El accidente</h2>
+        <div className="space-y-4 pt-6">
+          <h2 className="font-['Caveat'] text-5xl">El accidente</h2>
           <p>En los medios:</p>
           <div className="flex">
             <img src={Subrayado} alt="" className="w-20 rounded shadow mr-2" />
             <div>
-              <p>Diario Subrayado</p>
+              <p>
+                <strong>Diario Subrayado</strong>
+              </p>
               <p>
                 <a
-                  className="text-sky-600 underline text-xs"
+                  className="text-sky-600 underline"
                   href="https://www.subrayado.com.uy/patrullero-persecucion-choco-un-auto-el-barrio-ituzaingo-y-dejo-al-conductor-grave-n944208"
                   target="_blank"
                 >
@@ -72,10 +101,12 @@ const Hero = () => {
           <div className="flex">
             <img src={ElObservador} alt="" className="w-20 rounded shadow mr-2" />
             <div>
-              <p>Diario El Observador</p>
+              <p>
+                <strong>Diario El Observador</strong>
+              </p>
               <p>
                 <a
-                  className="text-sky-600 underline text-xs"
+                  className="text-sky-600 underline"
                   href="https://www.elobservador.com.uy/nota/un-movil-policial-que-seguia-a-una-moto-choco-contra-un-auto-en-barrio-ituzaingo-2024419124523"
                   target="_blank"
                 >
@@ -86,7 +117,7 @@ const Hero = () => {
           </div>
         </div>
         <div className="space-y-4 mt-6">
-          <h2 className="font-['Caveat'] text-4xl">Las secuelas</h2>
+          <h2 className="font-['Caveat'] text-5xl">Las secuelas</h2>
           <p>
             Mi papá es Técnico Electrónico. Siempre fue un hombre muy activo, trabajó toda su vida
             para darme lo mejor. Hoy, a más de 6 meses del accidente, sigue internado, con una
@@ -98,102 +129,123 @@ const Hero = () => {
             este estado me destroza...
           </p>
         </div>
+        <img src={Secuelas} className="mt-4 rounded shadow-md" />
         <div className="space-y-4 mt-6 pb-16">
-          <h2 className="font-['Caveat'] text-4xl">La recuperación</h2>
+          <h2 className="font-['Caveat'] text-5xl">La recuperación</h2>
           <p>
             El 19 de septiembre de 2024 nos visitó un médico especialista del{" "}
             <a href="https://www.fleni.org.ar/" className="text-sky-600 underline" target="_blank">
               centro Fleni
             </a>
             . El mismo diseñó un plan de rehabilitación multidisiplinario de 3 meses que va a ayudar
-            a mi papá a recuperar calidad de vida. El costo total del tratamiento es de $71.756.170
-            de pesos argentinos.
+            a mi papá a recuperar calidad de vida.{" "}
+            <span className="bg-yellow-300">
+              <strong>El costo total del tratamiento es de $71.756.170 de pesos argentinos</strong>
+            </span>
+            .
           </p>
           <p>
             Es una cifra que está muy por encima de nuestras posibilidades económicas, pero tengo la
             esperanza de que si logro juntar ese dinero para su tratamiento, la calidad de vida de
             mi papá pueda mejorar considerablemente.
           </p>
-          <p>
-            <a href="#" className="flex items-center text-xs underline text-red-700">
-              <FaRegFilePdf className="mr-1" /> Descargar Plan de Tratamiento de Fleni
-            </a>
+          <p
+            onClick={downloadPDF}
+            className="flex items-center underline text-red-700 cursor-pointer"
+          >
+            <FaRegFilePdf className="mr-1" /> Descargar Plan de Tratamiento de Fleni
           </p>
         </div>
       </div>
       <div className="flex-no-wrap fixed bottom-0 flex w-full items-center justify-center pb-4 z-10">
-        <button className="bg-sky-600 px-6 py-2 text-white font-bold rounded text-lg md:hidden flex justify-center items-center shadow-md">
+        <button
+          onClick={handleShow}
+          className="bg-sky-600 px-6 py-2 text-white font-bold rounded text-lg flex justify-center items-center shadow-md"
+        >
           Donar <FaHeart className="ml-2" />
         </button>
       </div>
-      <div className="bg-white/80 backdrop-blur w-full h-full fixed z-20 top-0 left-0 overflow-y-auto">
-        <div className="container h-full p-4 text-sm flex flex-col justify-center gap-6">
-          <div>
-            <h2 className="text-xl font-bold flex items-center">
-              <span class="fi fi-ar mr-2 rounded-sm shadow" /> Donaciones en Argentina
-            </h2>
-            <div className="px-3 py-2 my-4 bg-white text-base shadow">
-              <p>
-                <strong>Alias:</strong> mi.papa.richard
-              </p>
-              <p>
-                <strong>CVU:</strong> 0000003100060243814400
-              </p>
-              <p>
-                <strong>Maria Agustina Alonso</strong>
-              </p>
-              <p>
-                <strong>CUIT/CUIL:</strong> 27461367416
-              </p>
-            </div>
-            <a
-              href="https://link.mercadopago.com.ar/zevumara"
-              className="bg-[#009ee3] border border-black/20 px-6 py-2 text-white font-bold rounded text-lg md:hidden flex justify-center items-center shadow-md"
+      {isVisible && (
+        <div className="bg-white/80 backdrop-blur w-full h-full fixed z-20 top-0 left-0 overflow-y-auto">
+          <div className="container h-full p-4 text-sm flex flex-col justify-center gap-6 relative max-w-[720px]">
+            <div
+              onClick={handleClose}
+              className="bg-black text-white cursor-pointer text-center absolute top-4 right-4 px-2 pt-1 pb-1 opacity-50 rounded"
             >
-              Donar con Mercado Pago <FaHeart className="ml-2" />
-            </a>
-          </div>
-          <div>
-            <h2 className="text-xl font-bold flex items-center">
-              <span class="fi fi-uy mr-2 rounded-sm shadow" /> Donaciones en Uruguay
-            </h2>
-            <div className="px-3 py-2 my-4 bg-white text-base shadow">
-              <p>
-                <strong>Alias:</strong> mi.papa.richard
-              </p>
-              <p>
-                <strong>CVU:</strong> 0000003100060243814400
-              </p>
-              <p>
-                <strong>Maria Agustina Alonso</strong>
-              </p>
-              <p>
-                <strong>CUIT/CUIL:</strong> 27461367416
-              </p>
+              Volver
             </div>
-            <a
-              href="https://donafacil.uy/historia/un-camino-de-esperanza-para-richard-ayudemos-a-su-rehabilitacion-20241016155403"
-              className="bg-[#ff7db2] border border-black/20 px-6 py-2 text-white font-bold rounded text-lg md:hidden flex justify-center items-center shadow-md"
-            >
-              Donar con DonaFacil <FaHeart className="ml-2" />
-            </a>
-          </div>
-          <div>
-            <h2 className="text-xl font-bold flex items-center">
-              <span class="fi fi-eu mr-2 rounded-sm shadow" /> Donaciones internacionales
-            </h2>
-            <div className="px-3 py-2 my-4 bg-white text-base shadow">
-              <strong>PayPal:</strong> @mipaparichard
+            <div>
+              <h2 className="text-xl font-bold flex items-center">
+                <span class="fi fi-ar mr-2 rounded-sm shadow" /> Donaciones en Argentina
+              </h2>
+              <div className="px-3 py-2 my-4 bg-white text-base shadow">
+                <p>
+                  <strong>Alias:</strong> mi.papa.richard
+                </p>
+                <p>
+                  <strong>CVU:</strong> 0000003100060243814400
+                </p>
+                <p>
+                  <strong>Maria Agustina Alonso</strong>
+                </p>
+                <p>
+                  <strong>CUIT/CUIL:</strong> 27461367416
+                </p>
+              </div>
+              <a
+                href="https://link.mercadopago.com.ar/donacionesrichard"
+                target="_blank"
+                className="bg-[#009ee3] border border-black/20 px-6 py-2 text-white font-bold rounded text-lg flex justify-center items-center shadow-md"
+              >
+                Donar con Mercado Pago <FaHeart className="ml-2" />
+              </a>
             </div>
-            <a
-              href="https://www.paypal.com/donate/?hosted_button_id=KG42TENANUQ2N"
-              className="bg-[#ffc43b] border border-black/20 px-6 py-2 font-bold rounded text-lg md:hidden flex justify-center items-center shadow-md"
-            >
-              Donar con PayPal <FaHeart className="ml-2" />
-            </a>
+            <div>
+              <h2 className="text-xl font-bold flex items-center">
+                <span class="fi fi-uy mr-2 rounded-sm shadow" /> Donaciones en Uruguay
+              </h2>
+              <div className="px-3 py-2 my-4 bg-white text-base shadow">
+                <p>
+                  <strong>Número de cuenta:</strong> 11059473900001
+                </p>
+                <p>
+                  <strong>Banco:</strong> Banco República (BROU)
+                </p>
+                <p>
+                  <strong>Maria Agustina Alonso Kornicki</strong>
+                </p>
+              </div>
+              <a
+                href="https://donafacil.uy/historia/un-camino-de-esperanza-para-richard-ayudemos-a-su-rehabilitacion-20241016155403"
+                className="bg-[#ff7db2] border border-black/20 px-6 py-2 text-white font-bold rounded text-lg flex justify-center items-center shadow-md"
+              >
+                Donar con DonaFacil <FaHeart className="ml-2" />
+              </a>
+            </div>
+            <div>
+              <h2 className="text-xl font-bold flex items-center">
+                <span class="fi fi-eu mr-2 rounded-sm shadow" /> Donaciones internacionales
+              </h2>
+              <div className="px-3 py-2 my-4 bg-white text-base shadow">
+                <strong>PayPal:</strong>{" "}
+                <a
+                  href="https://paypal.me/mipaparichard"
+                  target="_blank"
+                  className="text-sky-600 underline"
+                >
+                  @mipaparichard
+                </a>
+              </div>
+              <a
+                href="https://www.paypal.com/donate/?hosted_button_id=CHJAGQ9WULQ44"
+                className="bg-[#ffc43b] border border-black/20 px-6 py-2 font-bold rounded text-lg flex justify-center items-center shadow-md"
+              >
+                Donar con PayPal <FaHeart className="ml-2" />
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,25 +28,11 @@ const NavbarLinks = [
 const Navbar = () => {
   return (
     <>
-      <header className="container py-2 px-6 flex justify-center md:justify-between items-center">
+      <header className="container py-2 px-6 flex justify-center items-center max-w-[720px]">
         <div className="flex justify-center items-center flex-col">
           <img src={Logo} alt="Logo corazón" className="w-2/4" />
-          <h1 className="font-['Caveat'] text-3xl">Mi papá Richard</h1>
+          <h1 className="font-['Caveat'] text-5xl">Mi papá Richard</h1>
         </div>
-        <nav className="hidden md:block">
-          <ul className="flex gap-3">
-            {NavbarLinks.map((link) => {
-              return (
-                <li key={link.id}>
-                  <a href={link.link}>{link.title}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-        <button className="bg-sky-600 px-6 py-2 text-white rounded font-medium hidden md:block">
-          Donar <FaHeart />
-        </button>
       </header>
     </>
   );
